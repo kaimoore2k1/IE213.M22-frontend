7b8c71c1fbc5a95f697f4a02f2b507648fa14943
Kai Moore <haohao12012001@gmail.com>
2022-04-13 15:28:44 +0700

    Edit Comment Ant Design Class Component to Function Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './sass/App.scss';
 import Contact from './modules/Home/Contact';
 import Pay from './pages/Pay';
+import Cmt from './components/core/Cmt';
 import 'antd/dist/antd.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      {/* <Contact /> */}
       {/* <Pay /> */}
+      <Cmt />
     </div>
   );
 }
