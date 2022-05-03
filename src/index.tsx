110b57fa6fdd28d8df58b70d2994f2189a3de518
Kai Moore <haohao12012001@gmail.com>
2022-05-03 14:37:09 +0700

    deploy backend

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  uri: "https://senshopdottech.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
--- src/pages/Store/Store.tsx
+++ src/pages/Store/Store.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Row, Col } from "antd";
 import { useLocation } from "react-router-dom";
 import { Category, ProductCategorySection } from "../../components/core";
